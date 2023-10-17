38e925de2a14162b1f27e0522178207bae339e4c
anna-hub19 <annabastosbeatriz@gmail.com>
2023-10-17 10:42:21 -0300

    aula 2

---
+++ src/paginas/cadastro/SelecaoCliente.jsx
@@ -0,0 +1,42 @@
+import { Col, Row } from "react-grid-system";
+import { Tipografia } from "../../componentes/Tipografia/Tipografia";
+import cliente from "../cadastro/assets/cliente.png"
+import freela from "../cadastro/assets/freela.png"
+import { Link } from "../../componentes/Link/Link"
+
+const SelecaoCliente = () => {
+ return(
+    <>
+    <Tipografia variante="h1" componente="h1">
+            Crie seu cadastro
+    </Tipografia>
+    <Tipografia variante="h3" componente="h2">
+           Como podemos te ajudar?
+    </Tipografia>
+    <Row>
+        <Col md={6} sm={12}>
+            <img src={cliente} alt="cliente"/>
+            <Tipografia variante="body" componente="body">
+                    Sou cliente e preciso de um freela!
+                </Tipografia>
+        </Col>
+        <Col md={6} sm={12}>
+            <img src={freela} alt="freelancer"/>
+            <Tipografia variante="body" componente="body">
+                    Sou um freela e preciso de clientes!
+                </Tipografia>
+        </Col>
+    </Row>
+    <div style={{ textAlign: 'center' }}>
+            <Tipografia variante="body2" componente="body2">
+                Já tem conta?
+            </Tipografia>
+            <p>
+                <Link variante="secundaria">Faça login!</Link>
+            </p>
+        </div>
+    </>
+ )
+}
+
+export default SelecaoCliente;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,37 @@
 import { createBrowserRouter } from "react-router-dom"
 import PaginaInicial from "../paginas/PaginaInicial"
+import SelecaoCliente from "../paginas/cadastro/SelecaoCliente";
+import LayoutBaseCadastro from "../paginas/cadastro/LayoutBaseCadastro";
+import LayoutBase from "../paginas/LayoutBase";
 
 export const router = createBrowserRouter([
-    {
+     {
         path: "/",
-        element: <PaginaInicial />,
-        children: [],
-    },
+        element: <LayoutBase/>,
+        children: [
+           
+          {
+                path:'cadastro',
+                element: <LayoutBaseCadastro />,
+                children: [
+                    {
+                        path: '',
+                        element: <SelecaoCliente />
+                    },
+                    {
+                    path: 'cliente',
+                    element: <h1> Interesses </h1>
+                },
+                {
+                    path: 'dados-pessoais',
+                    element: <h1> dados pessoais </h1>
+                },
+                {
+                    path: 'concluido',
+                    element: <h1> concluido </h1>
+                }
+               ]
+          }
+        ],
+      },
 ]);
